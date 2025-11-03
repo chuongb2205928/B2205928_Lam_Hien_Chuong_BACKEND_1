adb1634fd2eb313b011bea6b54c3e494f2634fd5
chuongb2205928 <chuongb2205928@student.ctu.edu.vn>
2025-11-03 21:42:34 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -8,4 +8,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
+
+const contactsRouter = require("./app/routes/contact.route");
+app.use("/api/contacts", contactsRouter);
+
 module.exports = app;
